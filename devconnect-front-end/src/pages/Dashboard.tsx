27bb67a72608b7baf58aef246cb1274d60211a7d
Hunter Brooks <hunterbrooks@MacBook-Air.local>
2025-03-16 15:37:16 -0600

    test

--- devconnect-front-end/src/pages/Dashboard.tsx
+++ devconnect-front-end/src/pages/Dashboard.tsx
@@ -1,8 +1,13 @@
+import "./Dashboard.css";
+
 const Dashboard = () => {
     return (
         <>
-        <h1>Welcome to Dev Connect</h1>
-        <p>Where Software Devs can connect with each other!</p>
+        <div className="dashboard-div">
+            <img src="./images/devconnect-logo.png"></img>
+            <h1>Welcome to Dev Connect</h1>
+            <p>Where Software Developers can connect with each other!</p>
+        </div>
         </>
     )
 }
--- devconnect-front-end/src/pages/Login.tsx
+++ devconnect-front-end/src/pages/Login.tsx
@@ -1,6 +1,26 @@
+import { Link } from "react-router-dom";
+import "./Login.css"
+
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <>
+            <form>
+                <div className="container">
+                    <label htmlFor="username"><b>UserName</b></label>
+                    <input type="text" placeholder="Enter Username" name="username" required></input>
+
+                    <label htmlFor="password"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="password" required></input>
+
+                    <button type="submit">Login</button>
+                </div>
+
+                <div className="container">
+                    <button type="button" className="cancelbutton">Cancel</button>
+                    <span className="password">Forgot Password</span>
+                </div>
+            </form>
+        </>
     )
 }
 
