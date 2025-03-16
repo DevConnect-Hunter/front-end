59e61974695016a5d64e1bdace71b6d8e441b7d8
Hunter Brooks <hunterbrooks@MacBook-Air.local>
2025-03-16 15:41:53 -0600

    test

--- devconnect-front-end/src/pages/Register.tsx
+++ devconnect-front-end/src/pages/Register.tsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+import "./Register.css";
+
 const Register = () => {
     return (
-        <h1>Register</h1>
+        <>
+            <form>
+                <div className="container">
+                    <label htmlFor="username"><b>UserName</b></label>
+                    <input type="text" placeholder="Create Username" name="username" required></input>
+
+                    <label htmlFor="password"><b>Password</b></label>
+                    <input type="password" placeholder="Create Password" name="password" required></input>
+
+                    <button type="submit">Create Account</button>
+                </div>
+            </form>
+        </>
     )
 }
 
